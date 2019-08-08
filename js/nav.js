ec4d1ee99c7a7b45b7c77c5726e5f1b3b0590986
Kris Renaldi <krenaldi@comcast.net>
2019-08-08 00:13:50 -0400

    Updated js script so that navbar hides once one of the menu items is clicked

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,10 @@
 const navToggle = document.querySelector('.menu-button');
+const nav = document.querySelector('nav');
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-is-open');
+})
+
+nav.addEventListener('click', () => {
+    document.body.classList.remove('nav-is-open');
 })
