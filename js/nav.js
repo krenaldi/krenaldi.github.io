66d8264e2ca0152f4df6bcbd7d7335e0a6f5c990
Kris Renaldi <krenaldi@comcast.net>
2019-08-15 01:33:42 -0400

    Modified contact form alignment and added close button to nav menu. Changed nav control buttons to inline SVG.

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,22 @@
 const navToggle = document.querySelector('.menu-button');
+const menuToggle = document.querySelector('.menu-caption');
+const closemenuToggle = document.querySelector('.close-caption');
+const closeToggle = document.querySelector('.menu-close');
 const nav = document.querySelector('nav');
 
 navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-is-open');
+})
+
+menuToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-is-open');
+})
+
+closemenuToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-is-open');
+})
+
+closeToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-is-open');
 })
 
