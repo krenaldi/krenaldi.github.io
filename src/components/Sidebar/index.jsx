87f8b9375067bfee72e923eaaeadb7444aff49ba
Kris Renaldi <krenaldi@comcast.net>
2023-09-16 16:03:29 -0400

    Completed and styled Sidebar component

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,15 +1,59 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './index.scss';
 import LogoS from '../../assets/images/logo-s.png';
 import LogoSub from '../../assets/images/logo_sub.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Sidebar = () => {
   return (
     <div className="nav-bar">
       <Link className="logo" to="/">
         <img src={LogoS} alt="logo" />
-        <img src={LogoSub} alt="subtitle logo" />
+        <img className="sub-logo" src={LogoSub} alt="subtitle logo" />
       </Link>
+      <nav>
+        <NavLink exact="true" to="/" activeClassName="active">
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          className="about-link"
+          exact="true"
+          to="/about"
+          activeClassName="active"
+        >
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          className="contact-link"
+          exact="true"
+          to="/contact"
+          activeClassName="active"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+      </nav>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/krenaldi/"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/krenaldi"
+          >
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
     </div>
   );
 };
