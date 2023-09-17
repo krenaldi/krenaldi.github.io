67f15a048b4523a91a1c22967c87e2e767fed147
Kris Renaldi <krenaldi@comcast.net>
2023-09-17 19:08:30 -0400

    Setup EmailJS service to send email from contact form

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,10 +1,33 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import Loader from 'react-loaders';
 import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
 
 const Contact = () => {
   const [letterClass, setLetterClass] = useState('text-animate');
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_7hrfl7e',
+        'template_rjn1m5w',
+        form.current,
+        'CSBVCfoV511qCKOUw'
+      )
+      .then(
+        () => {
+          alert('Message successfully sent!');
+          window.location.reload(false);
+        },
+        (error) => {
+          alert('Failed to send the message, please try again\n', error.text);
+        }
+      );
+  };
 
   useEffect(() => {
     setTimeout(() => {
@@ -29,7 +52,7 @@ const Contact = () => {
             any advice.
           </p>
           <div className="contact-form">
-            <form>
+            <form ref={form} onSubmit={sendEmail}>
               <ul>
                 <li className="half">
                   <input
