2adb6779957ad9a2351e103706eb21f1399e7b46
Kris Renaldi <krenaldi@comcast.net>
2023-09-16 23:47:39 -0400

    Added animated spinner with my skills in About me page

---
+++ src/components/About/index.jsx
@@ -0,0 +1,72 @@
+import { useEffect, useState } from 'react';
+import AnimatedLetters from '../AnimatedLetters';
+import './index.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngular, faCss3, faGitAlt, faHtml5, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons';
+
+const About = () => {
+  const [letterClass, setLetterClass] = useState('text-animate');
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover');
+    }, 3000);
+  }, [])
+
+  return (
+    <div className="container about-page">
+      <div className="text-zone">
+        <h1>
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={['A', 'b', 'o', 'u', 't', ' ', 'M', 'e']}
+            idx={15}
+          />
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam totam
+          odit id, animi, libero, velit rerum tenetur quod perferendis sapiente
+          quam mollitia soluta explicabo similique? Illo provident adipisci
+          aliquid unde!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam totam
+          odit id, animi, libero, velit rerum tenetur quod perferendis sapiente
+          quam mollitia soluta explicabo similique? Illo provident adipisci
+          aliquid unde!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam totam
+          odit id, animi, libero, velit rerum tenetur quod perferendis sapiente
+          quam mollitia soluta explicabo similique? Illo provident adipisci
+          aliquid unde!
+        </p>
+      </div>
+
+      <div className="stage-cube-container">
+        <div className="cubespinner">
+          <div className="face1">
+            <FontAwesomeIcon icon={faHtml5} color="#f06529" />
+          </div>
+          <div className="face2">
+            <FontAwesomeIcon icon={faCss3} color="#28a4d9" />
+          </div>
+          <div className="face3">
+            <FontAwesomeIcon icon={faJsSquare} color="#efd81d" />
+          </div>
+          <div className="face4">
+            <FontAwesomeIcon icon={faReact} color="#5ed4f4" />
+          </div>
+          <div className="face5">
+            <FontAwesomeIcon icon={faGitAlt} color="#ec4d28" />
+          </div>
+          <div className="face6">
+            <FontAwesomeIcon icon={faAngular} color="#dd0031" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About
