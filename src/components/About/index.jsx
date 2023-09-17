0526371d0cc3bebaca076976e4b2c1ed8cad8b2c
Kris Renaldi <krenaldi@comcast.net>
2023-09-17 17:50:03 -0400

    Added pacman Loader to Home and About pages to appear when pages load

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -3,6 +3,7 @@ import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngular, faCss3, faGitAlt, faHtml5, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons';
+import Loader from 'react-loaders';
 
 const About = () => {
   const [letterClass, setLetterClass] = useState('text-animate');
@@ -14,58 +15,61 @@ const About = () => {
   }, [])
 
   return (
-    <div className="container about-page">
-      <div className="text-zone">
-        <h1>
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={['A', 'b', 'o', 'u', 't', ' ', 'M', 'e']}
-            idx={15}
-          />
-        </h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam totam
-          odit id, animi, libero, velit rerum tenetur quod perferendis sapiente
-          quam mollitia soluta explicabo similique? Illo provident adipisci
-          aliquid unde!
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam totam
-          odit id, animi, libero, velit rerum tenetur quod perferendis sapiente
-          quam mollitia soluta explicabo similique? Illo provident adipisci
-          aliquid unde!
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam totam
-          odit id, animi, libero, velit rerum tenetur quod perferendis sapiente
-          quam mollitia soluta explicabo similique? Illo provident adipisci
-          aliquid unde!
-        </p>
-      </div>
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['A', 'b', 'o', 'u', 't', ' ', 'M', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+            totam odit id, animi, libero, velit rerum tenetur quod perferendis
+            sapiente quam mollitia soluta explicabo similique? Illo provident
+            adipisci aliquid unde!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+            totam odit id, animi, libero, velit rerum tenetur quod perferendis
+            sapiente quam mollitia soluta explicabo similique? Illo provident
+            adipisci aliquid unde!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+            totam odit id, animi, libero, velit rerum tenetur quod perferendis
+            sapiente quam mollitia soluta explicabo similique? Illo provident
+            adipisci aliquid unde!
+          </p>
+        </div>
 
-      <div className="stage-cube-container">
-        <div className="cubespinner">
-          <div className="face1">
-            <FontAwesomeIcon icon={faHtml5} color="#f06529" />
-          </div>
-          <div className="face2">
-            <FontAwesomeIcon icon={faCss3} color="#28a4d9" />
-          </div>
-          <div className="face3">
-            <FontAwesomeIcon icon={faJsSquare} color="#efd81d" />
-          </div>
-          <div className="face4">
-            <FontAwesomeIcon icon={faReact} color="#5ed4f4" />
-          </div>
-          <div className="face5">
-            <FontAwesomeIcon icon={faGitAlt} color="#ec4d28" />
-          </div>
-          <div className="face6">
-            <FontAwesomeIcon icon={faAngular} color="#dd0031" />
+        <div className="stage-cube-container">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faHtml5} color="#f06529" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faCss3} color="#28a4d9" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faJsSquare} color="#efd81d" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ed4f4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faGitAlt} color="#ec4d28" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faAngular} color="#dd0031" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <Loader type="pacman" />
+    </>
   );
 }
 
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -5,6 +5,7 @@ import AnimatedLetters from '../AnimatedLetters';
 import LogoTitle from '../../assets/images/logo-s.png';
 import Logo from './Logo';
 import './index.scss';
+import Loader from 'react-loaders';
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate');
@@ -33,34 +34,37 @@ const Home = () => {
 
 
   return (
-    <div className="container home-page">
-      <div className="text-zone">
-        <h1>
-          <span className={letterClass}>H</span>
-          <span className={`${letterClass} _12`}>i,</span>
-          <br />
-          <span className={`${letterClass} _13`}>I</span>
-          <span className={`${letterClass} _14`}>'m</span>
-          <img src={LogoTitle} alt="developer" />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={nameArray}
-            idx={15}
-          />
-          <br />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={jobArray}
-            idx={22}
-          />
-        </h1>
-        <h2>Full Stack Developer | Full Stack Coding Instructor & Mentor</h2>
-        <Link to="/contact" className="btn">
-          CONTACT ME
-        </Link>
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <img src={LogoTitle} alt="developer" />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
+          </h1>
+          <h2>Full Stack Developer | Full Stack Coding Instructor & Mentor</h2>
+          <Link to="/contact" className="btn">
+            CONTACT ME
+          </Link>
+        </div>
+        <Logo />
       </div>
-      <Logo />
-    </div>
+      <Loader type="pacman" />
+    </>
   );
 };
 
