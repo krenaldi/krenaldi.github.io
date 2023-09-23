cd8f14959f43c537a5564358b7bcd5365534d716
Kris Renaldi <krenaldi@comcast.net>
2023-09-23 18:20:00 -0400

    Styled Portfolio page for desktop view

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -28,6 +28,17 @@ const Portfolio = () => {
                 className="portfolio-image"
                 alt={port.title}
               />
+              <div className="content">
+                <p className="title">
+                  {port.title}
+                </p>
+                <h4 className="description">
+                  {port.description}
+                </h4>
+                <button className="btn" onClick={() => window.open(port.url)}>
+                  View
+                </button>
+              </div>
             </div>
           );
         })}
