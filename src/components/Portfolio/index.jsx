99177e63dfc076b75586c7b7ac41707db149e9a9
Kris Renaldi <krenaldi@comcast.net>
2023-09-23 17:34:46 -0400

    Added portfolio data file to be rendered on Portfolio page

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -1,16 +1,39 @@
 import { useEffect, useState } from 'react';
 import Loader from 'react-loaders';
 import AnimatedLetters from '../AnimatedLetters';
+import portfolioData from '../../data/portfolio.json';
 import './index.scss';
 
 const Portfolio = () => {
   const [letterClass, setLetterClass] = useState('text-animate');
 
+  console.log(portfolioData);
+
   useEffect(() => {
-    setTimeout(() => {
+    const timer = setTimeout(() => {
       setLetterClass('text-animate-hover');
     }, 3000);
+
+    return () => clearTimeout(timer);
   }, []);
+
+  const renderPortfolio = (portfolio) => {
+    return (
+      <div className="images-container">
+        {portfolio.map((port, idx) => {
+          return (
+            <div className="image-box" key={idx}>
+              <img
+                src={port.cover}
+                className="portfolio-image"
+                alt={port.title}
+              />
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
 
   return (
     <>
@@ -22,6 +45,7 @@ const Portfolio = () => {
             idx={15}
           />
         </h1>
+        <div>{renderPortfolio(portfolioData.portfolio)}</div>
       </div>
       <Loader type="pacman" />
     </>
