27278ea794ffbb971931752acae758494042ad9e
Kris Renaldi <krenaldi@comcast.net>
2023-09-24 16:13:26 -0400

    Made Sidebar responsive on mobile devices

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,19 +1,28 @@
+import { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import './index.scss';
 import LogoK from '../../assets/images/logo-K.png';
 import LogoSub from '../../assets/images/logo-sub-name.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faHome, faSuitcase, faUser } from '@fortawesome/free-solid-svg-icons';
+import {
+  faBars,
+  faClose,
+  faEnvelope,
+  faHome,
+  faSuitcase,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Sidebar = () => {
+  const [showNav, setShowNav] = useState(false);
   return (
     <div className="nav-bar">
       <Link className="logo" to="/">
         <img src={LogoK} alt="logo" />
         <img className="sub-logo" src={LogoSub} alt="subtitle logo" />
       </Link>
-      <nav>
+      <nav className={showNav ? 'mobile-show' : ''}>
         <NavLink exact="true" to="/" activeclassname="active">
           <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
         </NavLink>
@@ -41,6 +50,13 @@ const Sidebar = () => {
         >
           <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
         </NavLink>
+        <FontAwesomeIcon
+          icon={faClose}
+          color="#ffffff"
+          className="close-icon"
+          size="3x"
+          onClick={() => setShowNav(false)}
+        />
       </nav>
       <ul>
         <li>
@@ -62,6 +78,13 @@ const Sidebar = () => {
           </a>
         </li>
       </ul>
+      <FontAwesomeIcon
+        icon={faBars}
+        size="3x"
+        color="#ffffff"
+        className="hamburger-icon"
+        onClick={() => setShowNav(true)}
+      />
     </div>
   );
 };
